b58e815394f1b2c628a4e7ab0f8cf0e6a889a674
Pradana Yahya Abdillah <pradanayahyaabdillah@mail.ugm.ac.id>
2025-06-30 01:23:45 +0700

    add predict script

---
+++ scripts/predict.py
@@ -0,0 +1,36 @@
+import os
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'model'))
+
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForSequenceClassification.from_pretrained(model_dir)  
+
+model.eval()
+
+def model_predict(text: str):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    logits = outputs.logits
+    prediction = torch.argmax(logits, dim=1).item()
+    return 'sarcasm' if prediction == 1 else 'not sarcasm'
+
+def predict():
+    text = [
+        "Dia saking pinternya gatau cara bagi waktu",
+        "Kamu penulis ya? pandai sekali mengarang cerita"
+    ]
+    for i, text in enumerate(text, 1):
+        predicted_label = model_predict(text)
+        print(f"{i}. Text: '{text}' -> Predicted: {predicted_label}")
+        
+if __name__ == "__main__":
+    predict()
+    
+'''
+Output:
+1. Text: 'Dia saking pinternya gatau cara bagi waktu' -> Predicted: sarcasm
+2. Text: 'Kamu penulis ya? pandai sekali mengarang cerita' -> Predicted: sarcasm
+'''
